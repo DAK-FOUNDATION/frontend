fd3a5bc3e202d3f2782a2f1237d02b59e3dd231d
cuonghx <cuong1181998@gmail.com>
2025-10-17 14:25:46 +0700

    Update scripts for docker build

--- tools/scripts/build.docker.sh
+++ tools/scripts/build.docker.sh
@@ -5,4 +5,7 @@ rm -rf ./public/assets/configs
 rm -rf ./public/assets/multichain
 rm -rf ./public/assets/envs.js
 
-docker build --progress=plain --build-arg GIT_COMMIT_SHA=$(git rev-parse --short HEAD) --build-arg GIT_TAG=$(git describe --tags --abbrev=0) -t blockscout-frontend:local ./
+docker buildx create --use --name multiarch >/dev/null 2>&1 || docker buildx use multiarch
+docker buildx inspect --bootstrap
+
+docker build --progress=plain --platform=linux/amd64 --build-arg GIT_COMMIT_SHA=$(git rev-parse --short HEAD) --build-arg GIT_TAG=$(git describe --tags --abbrev=0) -t cuonghxdak/blockscout-frontend:v1.0.1 ./
